801f794301c824e2eac9841f6b9d5d146ca5c7f6
Daniel Canoy <canoydaniel06@gmail.com>
2025-11-19 16:15:12 +0800

    initial commit

---
+++ prisma/migrations/20251119081216_initial_migration/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
